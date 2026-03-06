63272d5560a7402ae3a854fc5c6c5fde27858fda
CesarVictor <cesarvictor@hotmail.fr>
2026-03-06 11:02:55 +0100

    Init transport

---
+++ backend/src/domain/sequencing/transport.rs
@@ -0,0 +1,33 @@
+#[derive(PartialEq, Clone, Debug)]
+enum TransportState {
+    Stopped,
+    Playing,
+}
+
+impl TransportState {
+    fn play(&mut self) -> Result<(), String> {
+        if self.is_playing() {
+            Err("Already playing".to_string())
+        } else {
+            *self = TransportState::Playing;
+            Ok(())
+        }
+    }
+
+    fn stop(&mut self) -> Result<(), String> {
+        if self.is_stopped() {
+            Err("Already stopped".to_string())
+        } else {
+            *self = TransportState::Stopped;
+            Ok(())
+        }
+    }
+
+    fn is_playing(&self) -> bool {
+        *self == TransportState::Playing
+    }
+    
+    fn is_stopped(&self) -> bool {
+        *self == TransportState::Stopped
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 mod ipc;
+mod domain;
 
 fn main() {
     ipc::run().expect("Failed to run IPC server");
